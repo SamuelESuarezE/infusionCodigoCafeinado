7680c2f4aa3ae7007553ffeda3517a00c56aa203
SamuelESuarezE <151666245+SamuelESuarezE@users.noreply.github.com>
2024-04-19 12:51:35 -0500

    feat: :construction: Asincronia getAllProductBill

--- js/bill.js
+++ js/bill.js
@@ -1,1 +1,26 @@
-import {example} from "./component/shopBillCamper.js"
+import {getAllProductBill} from "./component/shopBillCamper.js"
+
+let myBill = document.querySelector("#myBill")
+let data = await getAllProductBill()
+let row = ""
+
+data.forEach((val, id) => {
+    val.products.forEach(product => {
+        row += `
+            <tr>
+            <td>${product.description}</td>
+            <td>${product.quantity}</td>
+            <td>${product.price}</td>
+            <td>${product.total}</td> 
+            </tr>
+            `
+    })
+});
+myBill.innerHTML = row
+
+{/* <tr>
+<td>Incluye versión desktop y mobile</td>
+<td>2</td>
+<td>100 €</td>
+<td>200 €</td>
+</tr> */}
--- js/component/shopBillCamper.js
+++ js/component/shopBillCamper.js
@@ -1,3 +1,5 @@
-export const example = ()=> {
-    return "Hello world"
+export const getAllProductBill = async()=> {
+    let connection = await fetch("http://localhost:5600/camper")
+    let data = await connection.json()
+    return data
 }
